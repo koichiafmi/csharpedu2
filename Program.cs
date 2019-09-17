ec9452c0bd2d634841c54cb5e454fb184cbccae2
Koichiro Oyamada Main <koichi19791217@gmail.com>
2019-09-18 01:23:15 +0900

    解説用にクラスダイアグラム追加とちょこっと修正

--- Program.cs
+++ Program.cs
@@ -6,13 +6,17 @@ namespace TodohukenShiritori
 {
     class Program
     {
+        private static Word startWord;
+        private static WordChainSolver wc;
+        private static WordChainList ans;
+
         static void Main(string[] args)
         {
             DateTime dt = DateTime.Now;
             System.Collections.Generic.List<Word> list = WordsFile.GetWordList(120);
-            Word startWord = list[0];
-            WordChainSolver wc = new WordChainSolver(list);
-            WordChainList ans = wc.Solve(startWord);
+            startWord = list[0];
+            wc = new WordChainSolver(list);
+            ans = wc.Solve(startWord);
             ans.GetWordList().ToList().ForEach(w => Console.WriteLine(w.Text));
             Console.WriteLine("{0}", ans.GetCount());
             Console.WriteLine(DateTime.Now - dt);
